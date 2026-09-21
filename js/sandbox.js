32bea498689e3df1267f6ce93465778d5e6b569f
Mitsuba38JGR <mitsuba3838tossy@gmail.com>
2026-09-21 19:23:12 +0900

    Add sandbox functionality to execute code in iframe

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,1 +1,15 @@
+const iframe = document.getElementById("sandbox");
+
+document.getElementById("run").onclick = () => {
+    const code = editor.getValue();
+    iframe.contentWindow.postMessage({ code }, "*");
+};
+
+window.addEventListener("message", (event) => {
+    if (event.data.error) {
+        console.error("Sandbox Error:", event.data.error);
+    } else {
+        console.log("Sandbox Result:", event.data.result);
+    }
+});
 
