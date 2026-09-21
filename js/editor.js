0f3164e545661382a288127084a0b8e124078e64
Mitsuba38JGR <mitsuba3838tossy@gmail.com>
2026-09-21 20:26:46 +0900

    Initialize Monaco editor with default code and settings

--- js/editor.js
+++ js/editor.js
@@ -1,1 +1,20 @@
+let editor;
 
+require(["vs/editor/editor.main"], function () {
+    editor = monaco.editor.create(document.getElementById("editor"), {
+        value: `function rotate(piece, board) {
+    piece.rotation = (piece.rotation + 1) % 4;
+    return piece;
+}`,
+        language: "javascript",
+        theme: "vs-dark", // VSCode Dark+
+        automaticLayout: true,
+        fontSize: 15,
+        minimap: { enabled: false },
+        scrollBeyondLastLine: false,
+    });
+});
+
+export function getEditorCode() {
+    return editor.getValue();
+}
